655f7b04f20881713f237b0895743807b05da64d
Yota Toyama <raviqqe@gmail.com>
2019-04-15 15:51:15 +0000

    Add an example of free by borrow check

---
+++ examples/borrow_check_free/src/main.rs
@@ -0,0 +1,16 @@
+extern crate bdwgc_alloc;
+
+use bdwgc_alloc::Allocator;
+
+#[global_allocator]
+static GLOBAL_ALLOCATOR: Allocator = Allocator;
+
+fn main() {
+    unsafe { Allocator::initialize() }
+
+    let mut _n: Box<[u8; 2 ^ 8]> = Box::new([0; 2 ^ 8]);
+
+    loop {
+        _n = Box::new([0; 2 ^ 8]);
+    }
+}
