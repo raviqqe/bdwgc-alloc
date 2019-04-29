a63a24d4d2c8629c33bd121d286fed8c92bc4ac2
Yota Toyama <raviqqe@gmail.com>
2019-04-29 15:46:01 +0000

    Add coroutine support

---
+++ examples/coroutines/src/main.rs
@@ -0,0 +1,28 @@
+extern crate bdwgc_alloc;
+extern crate coroutine;
+
+use bdwgc_alloc::Allocator;
+use coroutine::asymmetric::Coroutine;
+use std::alloc::{GlobalAlloc, Layout};
+
+#[global_allocator]
+static GLOBAL_ALLOCATOR: Allocator = Allocator;
+
+fn main() {
+    unsafe {
+        Allocator::initialize();
+        Allocator::disable_gc();
+    }
+
+    let handle = Coroutine::spawn(move |_, _| {
+        let bottom: u8 = 0;
+        unsafe { Allocator::set_stack_bottom(&bottom) }
+        Allocator::enable_gc();
+
+        loop {
+            unsafe { GLOBAL_ALLOCATOR.alloc(Layout::from_size_align(2 ^ 8, 8).unwrap()) };
+        }
+    });
+
+    for _ in handle {}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate libc;
 
 mod error;
 
-use libc::{c_int, c_void, size_t};
+use libc::{c_char, c_int, c_void, size_t};
 use std::alloc::{GlobalAlloc, Layout};
 
 const GC_SUCCESS: c_int = 0;
@@ -16,17 +16,28 @@ struct GcStackBase {
 #[link(name = "gc", kind = "static")]
 extern "C" {
     fn GC_allow_register_threads() -> c_void;
+    fn GC_disable() -> c_void;
+    fn GC_enable() -> c_void;
     fn GC_free(ptr: *mut c_void);
     fn GC_get_stack_base(stack_base: *mut GcStackBase) -> c_int;
     fn GC_init() -> c_void;
     fn GC_malloc(size: size_t) -> *mut c_void;
     fn GC_register_my_thread(stack_base: *const GcStackBase) -> c_int;
+    fn GC_set_stack_bottom(thread: *const c_void, stack_bottom: *const c_char);
     fn GC_unregister_my_thread();
 }
 
 pub struct Allocator;
 
 impl Allocator {
+    pub fn disable_gc() {
+        unsafe { GC_disable() };
+    }
+
+    pub fn enable_gc() {
+        unsafe { GC_enable() };
+    }
+
     pub unsafe fn initialize() {
         GC_init();
         GC_allow_register_threads();
@@ -45,6 +56,13 @@ impl Allocator {
         }
 
         Ok(())
+    }
+
+    pub unsafe fn set_stack_bottom(bottom: *const u8) {
+        GC_set_stack_bottom(
+            std::mem::transmute(nix::sys::pthread::pthread_self()),
+            std::mem::transmute(bottom),
+        );
     }
 
     pub unsafe fn unregister_current_thread() {
