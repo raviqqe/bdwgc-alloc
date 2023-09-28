9dd6bfd031aa2fdca87757f98571bc846b6e40c3
Yota Toyama <raviqqe@gmail.com>
2023-09-28 15:17:12 +1000

    Refactor test

--- examples/test.sh
+++ examples/test.sh
@@ -4,10 +4,9 @@ set -ex
 
 cargo build
 
-for cargo_file in */Cargo.toml
-do
-	target/debug/$(dirname $cargo_file) &
-	pid=$!
-	sleep 10
-	kill $pid
+for cargo_file in */Cargo.toml; do
+  target/debug/$(dirname $cargo_file) &
+  pid=$!
+  sleep 20
+  kill $pid
 done
