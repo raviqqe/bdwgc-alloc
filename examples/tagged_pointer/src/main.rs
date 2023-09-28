54029abd348dd828e2da475016dfcf399e98f071
Yota Toyama <raviqqe@gmail.com>
2023-09-28 12:22:20 +1000

    Test tagged pointer

---
+++ examples/tagged_pointer/src/main.rs
@@ -0,0 +1,21 @@
+extern crate bdwgc_alloc;
+
+use bdwgc_alloc::Allocator;
+use std::alloc::Layout;
+
+#[global_allocator]
+static GLOBAL_ALLOCATOR: Allocator = Allocator;
+
+fn main() {
+    unsafe { Allocator::initialize() }
+
+    let handle = std::thread::spawn(move || {
+        unsafe { Allocator::register_current_thread().unwrap() }
+
+        loop {
+            unsafe { std::alloc::alloc(Layout::from_size_align(2 ^ 8, 8).unwrap()) };
+        }
+    });
+
+    handle.join().unwrap();
+}
